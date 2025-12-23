875fdb575ea8019a993ce32595f1d3b6365c9646
dsgler <1832002325@qq.com>
2025-12-23 15:19:59 +0800

    faet: add 404 page

---
+++ src/views/notFound/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-result
+    status="404"
+    :subtitle="$t('Page not found.')"
+    class="h-screen flex flex-col justify-center"
+  >
+    <template #extra>
+      <a-space>
+        <a-button type="primary" @click="router.replace('/')">
+          返回主页
+        </a-button>
+      </a-space>
+    </template>
+  </a-result>
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+</script>
+
+<style scoped lang="less"></style>
